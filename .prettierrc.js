e2d65eca9a087bb9b1cb814b550b181d6310d085
Dedal378 <orlovetsfoto@gmail.com>
2022-03-24 21:33:14 +0300

    initial, change fav

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  semi: false, // использовать точку с запятой ('true')
+  singleQuote: true, // использовать одинарные кавычки ('false')
+  arrowParens: 'avoid', // x => x (always - example: (x) => x)
+  // trailingComma: 'all', // висящие запятые ('es5', 'es5|none|all')
+  // bracketSameLine: true, // перенос > на другую строку при длинной строке ('false')
+  // printWidth: 120, // длина переноса строки (80)
+}
---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# abort on errors
+# set -e
+
+# yarn build
+# cd dist
+
+# git init
+# git checkout -b main
+# git add -A
+# git commit -m 'deploy'
+
+#  if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:Dedal378/vue-alef.git main:gh-pages
+
+# or
+# git subtree push --prefix dist origin gh-pages
+
+# cd -
